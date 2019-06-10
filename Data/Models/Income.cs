f544270df31b5e5da7b40e6e8019595e538b60d4
Alek Hristov <alek.hristov@yahoo.com>
2019-06-10 23:56:18 +0300

    Income model added

--- Data/Models/Income.cs
+++ Data/Models/Income.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BudgetPlanner.Data.Models.Abstracts;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BudgetPlanner.Data.Models
 {
-    public class Income
+    public class Income : ModelBase
     {
+        public IncomeType Type { get; set; }
+
+        public decimal Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+
+        public int BudgetId { get; set; }
+
+        public Budget Budget { get; set; }
+    }
+
+    public enum IncomeType
+    {
+        SALARY,
+        RENT,
+        INTEREST,
+        DIVIDENDS,
+        BONUSES
     }
 }
--- Data/Models/Payment.cs
+++ Data/Models/Payment.cs
@@ -18,5 +18,9 @@ namespace BudgetPlanner.Data.Models
         public int CategoryId { get; set; }
 
         public Category Category { get; set; }
+
+        public int UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
     }
 }
