2db6745da3a32f975d607e1270d23bfc633da794
Alek Hristov <alek.hristov@yahoo.com>
2019-06-10 23:34:53 +0300

    User model created

---
+++ Data/Models/Abstracts/ModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetPlanner.Data.Models.Abstracts
+{
+    public class ModelBase
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ Data/Models/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿using BudgetPlanner.Data.Models.Abstracts;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetPlanner.Data.Models
+{
+    public class ApplicationUser : ModelBase
+    {
+        public ApplicationUser()
+        {
+            this.Budgets = new HashSet<Budget>();
+        }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 symbols")]
+        public string FirstName { get; set; }
+
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public int PaymentId { get; set; }
+
+        public Payment Payment { get; set; }
+
+        /// <summary>
+        /// Navigation property - represents related entity
+        /// </summary>
+        ICollection<Budget> Budgets { get; set; }
+    }
+
+    public enum Gender
+    {
+        MALE,
+        FEMALE
+    }
+}
