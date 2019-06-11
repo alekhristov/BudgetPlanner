cdc0ce4a271c6df4bc7296a9d651e322a4789581
Alek Hristov <alek.hristov@yahoo.com>
2019-06-11 23:57:27 +0300

    Payment service added

---
+++ Data/Services/Abstracts/IPaymentService.cs
@@ -0,0 +1,16 @@
+﻿using BudgetPlanner.Data.Models;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data.Services.Abstracts
+{
+    public interface IPaymentService
+    {
+        Task AddPayment(Payment payment);
+
+        Task DeletePayment(int paymentId);
+
+        Task EditPayment(Payment payment);
+
+        Task<Payment> GetPayment(int paymentId);
+    }
+}
---
+++ Data/Services/PaymentService.cs
@@ -0,0 +1,46 @@
+﻿using BudgetPlanner.Data.Models;
+using BudgetPlanner.Data.Services.Abstracts;
+using System;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly BudgetPlannerDbContext dbContext;
+
+        public PaymentService(BudgetPlannerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddPayment(Payment payment)
+        {
+            this.dbContext.Payments.Add(payment);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeletePayment(int paymentId)
+        {
+            var payment = await this.GetPayment(paymentId);
+
+            this.dbContext.Remove(payment);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task EditPayment(Payment payment)
+        {
+            await this.GetPayment(payment.Id);
+
+            this.dbContext.Update(payment);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Payment> GetPayment(int paymentId)
+        {
+            var payment = await this.dbContext.Payments.FindAsync(paymentId) ?? throw new NullReferenceException("There is no such payment!");
+
+            return payment;
+        }
+    }
+}
