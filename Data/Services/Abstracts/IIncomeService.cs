4486f457e06f992f4a8d09d7360c7ddedb198988
Alek Hristov <alek.hristov@yahoo.com>
2019-06-12 00:05:29 +0300

    Income service added

---
+++ Data/Services/Abstracts/IIncomeService.cs
@@ -0,0 +1,19 @@
+﻿using BudgetPlanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data.Services.Abstracts
+{
+    public interface IIncomeService
+    {
+        Task AddIncome(Income income);
+
+        Task DeleteIncome(int incomeId);
+
+        Task UpdateIncome(Income income);
+
+        Task<Income> GetIncome(int incomeId);
+    }
+}
---
+++ Data/Services/IncomeService.cs
@@ -0,0 +1,46 @@
+﻿using BudgetPlanner.Data.Models;
+using BudgetPlanner.Data.Services.Abstracts;
+using System;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data.Services
+{
+    public class IncomeService : IIncomeService
+    {
+        private readonly BudgetPlannerDbContext dbContext;
+
+        public IncomeService(BudgetPlannerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddIncome(Income income)
+        {
+            this.dbContext.Incomes.Add(income);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteIncome(int incomeId)
+        {
+            var income = this.GetIncome(incomeId);
+
+            this.dbContext.Remove(income);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Income> GetIncome(int incomeId)
+        {
+            var income = await this.dbContext.Incomes.FindAsync(incomeId) ?? throw new NullReferenceException("No such income exists");
+
+            return income;
+        }
+
+        public async Task UpdateIncome(Income income)
+        {
+            var incomeToUpdate = await this.GetIncome(income.Id);
+
+            this.dbContext.Incomes.Update(income);
+            await this.dbContext.SaveChangesAsync();
+        }
+    }
+}
