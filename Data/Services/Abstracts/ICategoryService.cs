389a6bf652022bb256e4aeabc698265c0cfa8938
Alek Hristov <alek.hristov@yahoo.com>
2019-06-11 23:46:25 +0300

    Added Category service

---
+++ Data/Services/Abstracts/ICategoryService.cs
@@ -0,0 +1,25 @@
+﻿using BudgetPlanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data.Services.Abstracts
+{
+    public interface ICategoryService
+    {
+        Task AddCategory(Category category);
+
+        Task AddCategoriesRange(ICollection<Category> categories);
+
+        Task<ICollection<Category>> GetAllCategories();
+
+        Task<Category> GetCategory(int categoryId);
+
+        Task EditCategory(Category category);
+
+        Task DeleteCategory(int categoryId);
+
+        Task DisableCategory(int categoryId);
+    }
+}
---
+++ Data/Services/CategoryService.cs
@@ -0,0 +1,69 @@
+﻿using BudgetPlanner.Data.Models;
+using BudgetPlanner.Data.Services.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Data.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly BudgetPlannerDbContext dbContext;
+
+        public CategoryService(BudgetPlannerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// For populating DB
+        /// </summary>
+        public async Task AddCategoriesRange(ICollection<Category> categories)
+        {
+            this.dbContext.Categories.AddRange(categories);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            this.dbContext.Categories.Add(category);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var category = await this.GetCategory(categoryId);
+
+            this.dbContext.Categories.Remove(category);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task DisableCategory(int categoryId)
+        {
+            var category = await this.GetCategory(categoryId);
+
+            category.IsDisabled = true;
+            await this.EditCategory(category);
+        }
+
+        public async Task EditCategory(Category category)
+        {
+            var categoryToUpdate = await this.dbContext.Categories.FindAsync(category.Id) ?? throw new NullReferenceException("No such category exists!");
+
+            this.dbContext.Categories.Update(category);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public Task<ICollection<Category>> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> GetCategory(int categoryId)
+        {
+            var category = await this.dbContext.Categories.FindAsync(categoryId) ?? throw new NullReferenceException("No such category exists!");
+
+            return category;
+        }
+    }
+}
